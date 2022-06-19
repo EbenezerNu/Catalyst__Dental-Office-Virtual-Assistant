e957af2f4b9ca830d90f360afd12778ac7d077f7
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2022-06-19 10:03:02 +0000

    Resolved the Configuration issue

--- starter/ContosoDentistryChatBot/index.js
+++ starter/ContosoDentistryChatBot/index.js
@@ -20,8 +20,10 @@ const { DentaBot } = require('./bot');
 // Create HTTP server
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${server.name} listening to ${server.url}`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(
+        '\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator'
+    );
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
@@ -37,25 +39,32 @@ const onTurnErrorHandler = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${error}`);
+    console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
-        `${error}`,
+        `${ error }`,
         'https://www.botframework.com/schemas/error',
         'TurnError'
     );
 
     // Send a message to the user
     await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    await context.sendActivity(
+        'To continue to run this bot, please fix the bot source code.'
+    );
 };
 
 // Set the onTurnError for the singleton BotFrameworkAdapter.
 adapter.onTurnError = onTurnErrorHandler;
 
 // Map configuration values values from .env file into the required format for each service.
+console.log(
+    'knowledgeBaseId: ' + process.env.QnAKnowledgebaseId,
+    'endpointKey: ' + process.env.QnAAuthKey,
+    'host: ' + process.env.QnAEndpointHostName
+);
 const QnAConfiguration = {
     knowledgeBaseId: process.env.QnAKnowledgebaseId,
     endpointKey: process.env.QnAAuthKey,
@@ -65,26 +74,28 @@ const QnAConfiguration = {
 const LuisConfiguration = {
     applicationId: process.env.LuisAppId,
     endpointKey: process.env.LuisAPIKey,
-    endpoint: process.env.LuisAPIHostName,
-}
+    endpoint: process.env.LuisAPIHostName
+};
 
 const SchedulerConfiguration = {
     SchedulerEndpoint: process.env.SchedulerEndpoint
-}
-//pack each service configuration into 
+};
+// pack each service configuration into
 const configuration = {
     QnAConfiguration,
     LuisConfiguration,
     SchedulerConfiguration
-}
+};
 
 // Create the main dialog.
+console.log('Configuration : ', configuration);
+
 const myBot = new DentaBot(configuration, {});
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
+    // Route to main dialog.
         await myBot.run(context);
     });
 });
@@ -100,8 +111,8 @@ server.on('upgrade', (req, socket, head) => {
     streamingAdapter.onTurnError = onTurnErrorHandler;
 
     streamingAdapter.useWebSocket(req, socket, head, async (context) => {
-        // After connecting via WebSocket, run this logic for every request sent over
-        // the WebSocket connection.
+    // After connecting via WebSocket, run this logic for every request sent over
+    // the WebSocket connection.
         await myBot.run(context);
     });
 });
